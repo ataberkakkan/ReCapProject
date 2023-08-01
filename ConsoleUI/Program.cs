40dcb189a72b6213a155b53d1105735541651946
Ataberk <Ataberk@LAPTOP-5A694671>
2023-08-01 13:37:58 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,9 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+CarManager carManager = new CarManager(new InMemoryCarDal());
+
+foreach (var car in carManager.GetAllByBrand(1))
+{
+    Console.WriteLine(car.Description);
+}
