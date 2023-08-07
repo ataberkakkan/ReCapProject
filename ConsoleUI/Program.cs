c6a1a7dc0d6acdcc8c29b4f9352673620b90417a
ataberkakkan <akkanataberk@gmail.com>
2023-08-07 10:32:13 +0300

    Day 9 Reqs

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -4,7 +4,7 @@ using Entities.Concrete;
 
 CarManager carManager = new CarManager(new EfCarDal());
 
-foreach (var car in carManager.GetCarsByColorId(1))
+foreach (var car in carManager.GetCarDetails())
 {
-    Console.WriteLine(car.CarName);
+    Console.WriteLine(car.CarName + " : " + car.BrandName + "  " + car.ColorName + "  " + car.DailyPrice);
 }
