04fd9778510e17715328d799d5cad325f2441fcd
ataberkakkan <akkanataberk@gmail.com>
2023-08-08 15:21:47 +0300

    HW 10

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -2,9 +2,5 @@
 using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
 
-CarManager carManager = new CarManager(new EfCarDal());
 
-foreach (var car in carManager.GetCarDetails().Data)
-{
-    Console.WriteLine(car.CarName + " : " + car.BrandName + "  " + car.ColorName + "  " + car.DailyPrice);
-}
+
